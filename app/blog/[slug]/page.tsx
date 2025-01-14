1bcb6cec92d24bb7975df4e4710e27d498e0c239
Ahamed Favas <89861677+Ahamed-Favas@users.noreply.github.com>
2025-01-14 16:26:37 +0530

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -111,7 +111,7 @@ async function Page({params}: {params: {slug: string}}) {
               </div>
             </nav>
             </aside>
-        <div className="w-full md:w-3/4">
+        <div className="w-full p-4 md:w-3/4">
           <h2 className="text-2xl font-bold mb-2">{article.title}</h2>
           <p className="text-sm text-gray-600 mb-2">{article.fomatedDate}</p>
           <div className="mt-10">{article.content}</div>
