9f6161bce7c1a18797d5b76f341048d90a25b0c6
Ahamed Favas <ahamedfavasedanatt@gmail.com>
2025-01-14 15:33:01 +0530

    changed about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Github, Linkedin } from "lucide-react";
 import Link from "next/link";
 
