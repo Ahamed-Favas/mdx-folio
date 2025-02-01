4d35ae636d111fe0c79562e9b1fca1d824169189
Ahamed Favas <89861677+Ahamed-Favas@users.noreply.github.com>
2025-02-01 21:54:27 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import BlogMenubarComponent from "@/components/BlogMenuBar"
+import BlogMenubarComponent from "@/components/BlogMenuBar";
 import { getArticle } from './utils';
 import Link from "next/link";
 
@@ -6,22 +6,25 @@ export default async function BlogPage() {
   const articleTitles = await getArticle();
   return (
     <div>
-      <br/>
+      <br />
       <BlogMenubarComponent />
-      <div className="space-y-4"> 
+      <div className="space-y-4">
         {articleTitles.map((article, index) => (
-  <article key={article.filename} className="hover:bg-gray-50 dark:hover:bg-gray-800 p-4 mt-4 rounded-s">
-    <Link href={`/blog/${article.filename}`}>
-      <h2 className="text-2xl font-semibold mb-2">{article.title}</h2>
-      <p className="text-sm hover:text-gray-50 hover:dark:text-gray-800 mb-2">{`Published on ${article.fomatedDate}`}</p>
-      <p>{`${article.summary.trim()}..`}</p>
-    </Link>
-  </article>
-{index !== articleTitles.length - 1 && (
-      <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-    )}
-))}
+          <article key={article.filename} className="hover:bg-gray-50 dark:hover:bg-gray-800 p-4 mt-4 rounded-s">
+            <Link href={`/blog/${article.filename}`}>
+              <h2 className="text-2xl font-semibold mb-2">{article.title}</h2>
+              <p className="text-sm hover:text-gray-50 hover:dark:text-gray-800 mb-2">
+                {`Published on ${article.fomatedDate}`}
+              </p>
+              <p>{`${article.summary.trim()}..`}</p>
+            </Link>
+          </article>
+        ))}
+        {articleTitles.length > 1 &&
+          articleTitles.slice(0, -1).map((_, index) => (
+            <hr key={index} className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+          ))}
       </div>
     </div>
-  )
+  );
 }
