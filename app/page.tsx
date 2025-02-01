48e8306f7fab9926d324fc17cff7573052f3405f
Ahamed Favas <89861677+Ahamed-Favas@users.noreply.github.com>
2025-02-01 21:32:22 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,18 @@ export default async function BlogPage() {
       <br/>
       <BlogMenubarComponent />
       <div className="space-y-4"> 
-        {articleTitles.map((article, index) =>
-            <article key={article.filename} className="bg-gray-100 dark:bg-gray-400 p-4 mt-4 rounded-s">
-            <Link href ={`blog/${article.filename}`}>
-              <h2 className="text-2xl font-semibold mb-2">{article.title}</h2>
-              <p className="text-sm hover:text-gray-50 hover:dark:text-gray-800 mb-2">{`Published on ${article.fomatedDate}`}</p>
-              <p>{`${article.summary}..`}</p>
-            </Link>
-            </article>
-       {index !== articleTitles.length - 1 && (
+        {articleTitles.map((article, index) => (
+  <article key={article.filename} className="bg-gray-100 dark:bg-gray-400 p-4 mt-4 rounded-s">
+    <Link href={`/blog/${article.filename}`}>
+      <h2 className="text-2xl font-semibold mb-2">{article.title}</h2>
+      <p className="text-sm hover:text-gray-50 hover:dark:text-gray-800 mb-2">{`Published on ${article.formattedDate}`}</p>
+      <p>{`${article.summary}..`}</p>
+    </Link>
+    {index !== articleTitles.length - 1 && (
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-    )} )}
+    )}
+  </article>
+))}
       </div>
     </div>
   )
