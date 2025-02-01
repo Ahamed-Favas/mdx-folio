df6dc5223802e029d2fa3a6ce7b91c56163e7109
Ahamed Favas <89861677+Ahamed-Favas@users.noreply.github.com>
2025-02-01 21:49:58 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,16 @@ export default async function BlogPage() {
       <BlogMenubarComponent />
       <div className="space-y-4"> 
         {articleTitles.map((article, index) => (
-  <article key={article.filename} className="bg-gray-100 dark:bg-gray-400 p-4 mt-4 rounded-s">
+  <article key={article.filename} className="hover:bg-gray-50 dark:hover:bg-gray-800 p-4 mt-4 rounded-s">
     <Link href={`/blog/${article.filename}`}>
       <h2 className="text-2xl font-semibold mb-2">{article.title}</h2>
       <p className="text-sm hover:text-gray-50 hover:dark:text-gray-800 mb-2">{`Published on ${article.fomatedDate}`}</p>
-      <p>{`${article.summary}..`}</p>
+      <p>{`${article.summary.trim()}..`}</p>
     </Link>
-    {index !== articleTitles.length - 1 && (
+  </article>
+{index !== articleTitles.length - 1 && (
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
     )}
-  </article>
 ))}
       </div>
     </div>
